799acfb1c474c129e2f9be9797b77caae256504e
IT24102821 <it24102821@my.sliit.lk>
2025-09-02 22:58:59 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24102821_Lab_06.R
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\nadil\\Downloads\\DDD")
+
+#Question 1
+
+#1
+#Binomial Distribution
+
+#2
+1-pbinom(47,50,0.85,lower.tail =FALSE)
+
+
+#Question 2
+
+#1
+#number of customer calls per hour
+
+#2
+#poisson Distribution
+
+#3
+dpois(15,12)
+
